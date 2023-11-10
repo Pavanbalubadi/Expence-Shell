54bbcbc8b6673323683df94c445912f80b7ae81e
Balubadi <balubadi.pavankumar@gmail.com>
2023-11-10 15:54:35 +0530

    frontend-dev config file config file frontend backend mysql chsnge

--- mysql-dev.sh
+++ mysql-dev.sh
@@ -18,3 +18,4 @@ systemctl start mysqld
 echo -e "\e[36m login to mysql \e[0m"
 
 mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql -h <mysql-dev.pavanbalubadi3017.online> -uroot -pExpenseApp@1 < /app/schema/backend.sql
