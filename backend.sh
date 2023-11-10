be82b7c0d9e0bb6fdebd091d52807a7061953d09
Balubadi <balubadi.pavankumar@gmail.com>
2023-11-10 16:05:11 +0530

    frontend-dev config file config file frontend backend mysql chsnge

--- backend.sh
+++ backend.sh
@@ -2,9 +2,10 @@ echo -e "\e[36m disable default vertion of nodjs \e[0m"
 
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
-cp backend.service /etc/systemd/system/backend.service
+
 echo -e "\e[36m install nodjs  \e[0m"
 dnf install nodejs -y
+cp backend.service /etc/systemd/system/backend.service
 
 echo -e "\e[36m adding the user \e[0m"
 useradd expense
@@ -15,8 +16,8 @@ cd /app
 unzip /tmp/backend.zip
 cd /app
 npm install
+dnf install mysql -y
+mysql -h <mysql-dev.pavanbalubadi3017.online> -uroot -pExpenseApp@1 < /app/schema/backend.sql
 systemctl daemon-reload
 systemctl enable backend
 systemctl start backend
-dnf install mysql -y
-mysql -h <mysql-dev.pavanbalubadi3017.online> -uroot -pExpenseApp@1 < /app/schema/backend.sql
