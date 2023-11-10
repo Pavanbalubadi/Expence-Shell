692c8585ad770c9bbb858bc79f83814540182933
Balubadi <balubadi.pavankumar@gmail.com>
2023-11-10 14:58:46 +0530

    frontend-dev config file config file

--- fronten.sh
+++ fronten.sh
@@ -1,15 +1,15 @@
 echo -e "\e[36m insatll nginx \e[0m"
 dnf install nginx -y
-
+echo -e "\e[36m copy expence.conf file \e[0m"
 cp expence.conf /etc/nginx/default.d/expense.conf
-
+echo -e "\e[36m remove nginx old content \e[0m"
 rm -rf /usr/share/nginx/html/*
-
+echo -e "\e[36m download frontend application \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
-
+echo -e "\e[36m unzip nginx \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-
+echo -e "\e[36m start the nginx \e[0m"
 systemctl enable nginx
 systemctl start nginx
 
