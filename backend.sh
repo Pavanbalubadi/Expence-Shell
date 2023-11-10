82fd5e6d51de56b19afc551da495e9dea01cb60d
Balubadi <balubadi.pavankumar@gmail.com>
2023-11-10 15:27:23 +0530

    frontend-dev config file config file frontend backend mysql

---
+++ backend.sh
@@ -0,0 +1,21 @@
+echo -e "\e[36m disable default vertion of nodjs \e[0m"
+
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+cp  /etc/systemd/system/backend.service /etc/systemd/system/backend.service
+echo -e "\e[36m install nodjs  \e[0m"
+dnf install nodejs -y
+
+echo -e "\e[36m adding the user \e[0m"
+useradd expense
+echo -e "\e[36m add app directory  \e[0m"
+mkdir /app
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+cd /app
+unzip /tmp/backend.zip
+cd /app
+npm install
+systemctl daemon-reload
+systemctl enable backend
+systemctl start backend
+dnf install mysql -y
