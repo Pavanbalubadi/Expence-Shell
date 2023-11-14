d32ed0053fb903eff9c848e036cbdeb15ffeda03
Balubadi <balubadi.pavankumar@gmail.com>
2023-11-14 21:58:27 +0530

    frontend-dev config file config file frontend backend mysql chsnge

--- backend.sh
+++ backend.sh
@@ -1,9 +1,12 @@
-log_file= "/temp/expence.log"
+log_file="/tmp/expense.log"
+color="\e[33m"
+
 status_check() {
-  if [ $? -eq 0 ];then
-    echo -e "\e[32m sucesses \e[0m"
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
   else
-    echo -e "\e[33m failure \e[0m"
+    echo -e "\e[31m FAILURE \e[0m"
+    exit 1
   fi
 }
 
